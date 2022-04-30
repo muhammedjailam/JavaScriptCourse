e37a9aee3e43ae7d4075a8acf726890fbf060f0d
muhammedjailam <muhammedjailam@gmail.com>
2022-04-30 20:38:31 +0500

    - Added Section 5 files

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'script.bundle.js',
+  },
+  module: {
+      rules: [{
+		test: /\.js$/,
+        exclude: /node_modules/,
+		use: {
+			loader: 'babel-loader',
+			options: {
+				presets: ["@babel/preset-env"]
+			}	
+		}
+      }]
+  },
+};
